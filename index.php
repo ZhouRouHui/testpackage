f4f67717ffeeffec7bbbaf0b83be67be1e173ff7
zhourouhui <zhourouhui@ahaschool.com>
2019-04-29 11:44:23 +0800

    update

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+class TestPackage {
+	public $name = 'zhang san';
+	public $age = 18;
+
+	funciton __constroct (string $name = '', int $age = 0) 
+	{
+		if (!empyt($name)) {
+			$this->name = $name;
+		}
+		if ($age > 0) {
+			$this->age = $age;
+		}
+	}
+
+	function getInfo () {
+		echo "my name is {$this->name}, i am {$this->age}";
+	}
+}
